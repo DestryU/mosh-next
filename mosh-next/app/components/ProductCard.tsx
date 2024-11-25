0ffc37113c52b718c986a05cfd1cd6c44b369f88
DestryU <47061782+DestryU@users.noreply.github.com>
2024-11-25 10:06:14 -0700

    Reviewed information on styling; as the part that I'm most comfortable with, this was largely review of comfortable topics.

--- mosh-next/app/components/ProductCard.tsx
+++ mosh-next/app/components/ProductCard.tsx
@@ -3,8 +3,8 @@ import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
-    <div>
-        <AddToCart />
+    <div className="p-4 my-4 bg-sky-400 text-white text-2xl hover:bg-sky-600">
+      <AddToCart />
     </div>
   );
 };
